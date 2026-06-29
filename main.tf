106a83c2f1c4a889e104aff90f3dfb130807c5ac
Pandu Rani <ranipandu340@gmail.com>
2026-06-29 11:52:06 +0530

    “terraform”

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    # source = "../terraform-aws-vpc"
+    source = "git::https://github.com/Pandu-sys/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = true
